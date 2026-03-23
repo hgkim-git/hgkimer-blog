3441acdc767f8afc775921922b137943ff459612
Hogyu,Kim <hgkimer@gmail.com>
2026-03-23 21:00:17 +0900

    chore(gradle): 시큐리티, jjwt 관련 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -92,6 +92,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("nz.net.ultraq.thymeleaf:thymeleaf-layout-dialect:3.4.0")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.thymeleaf.extras:thymeleaf-extras-springsecurity6")
     "developmentOnly"("org.springframework.boot:spring-boot-devtools")
 
     // Lombok
@@ -113,8 +115,14 @@ dependencies {
     // jsoup HTML parser library @ https://jsoup.org/
     implementation("org.jsoup:jsoup:1.22.1")
 
+    // jjwt
+    implementation("io.jsonwebtoken:jjwt-api:0.13.0")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.13.0")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.13.0")
+
     // Test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.security:spring-security-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
